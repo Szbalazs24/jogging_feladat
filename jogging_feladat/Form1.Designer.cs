8c65985fa942d393e9c4823efdd2e7d2cd411241
Szbalazs24 <szekely.balazs24@gmail.com>
2025-02-21 08:40:47 +0100

    elso_commit

---
+++ jogging_feladat/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace jogging_feladat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.o_ki = new System.Windows.Forms.Label();
+            this.kiiras = new System.Windows.Forms.Label();
+            this.tallozas = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(28, 98);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(191, 264);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Ázsia országai",
+            "Afrika országai",
+            "Európa országai"});
+            this.comboBox1.Location = new System.Drawing.Point(28, 71);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(110, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // o_ki
+            // 
+            this.o_ki.AutoSize = true;
+            this.o_ki.Location = new System.Drawing.Point(366, 24);
+            this.o_ki.Name = "o_ki";
+            this.o_ki.Size = new System.Drawing.Size(40, 13);
+            this.o_ki.TabIndex = 2;
+            this.o_ki.Text = "Ország";
+            // 
+            // kiiras
+            // 
+            this.kiiras.AutoSize = true;
+            this.kiiras.Location = new System.Drawing.Point(256, 170);
+            this.kiiras.Name = "kiiras";
+            this.kiiras.Size = new System.Drawing.Size(90, 13);
+            this.kiiras.TabIndex = 3;
+            this.kiiras.Text = "Választott ország!";
+            // 
+            // tallozas
+            // 
+            this.tallozas.Location = new System.Drawing.Point(144, 69);
+            this.tallozas.Name = "tallozas";
+            this.tallozas.Size = new System.Drawing.Size(75, 23);
+            this.tallozas.TabIndex = 4;
+            this.tallozas.Text = "Tallózás";
+            this.tallozas.UseVisualStyleBackColor = true;
+            this.tallozas.Click += new System.EventHandler(this.tallozas_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tallozas);
+            this.Controls.Add(this.kiiras);
+            this.Controls.Add(this.o_ki);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label o_ki;
+        private System.Windows.Forms.Label kiiras;
+        private System.Windows.Forms.Button tallozas;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ jogging_feladat/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jogging_feladat
+{
+
+    
+
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public List<orszagok> orszag = new List<orszagok>();
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            orszag.Clear();
+            listBox1.Items.Clear();
+            string a = "";
+            if (comboBox1.SelectedIndex == 0)
+            {
+                a = "forras_azsia.txt";
+                o_ki.Text = "Ázsia";
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                a = "forras_afrika.txt";
+                o_ki.Text = "Afrika";
+            }
+            else if (comboBox1.SelectedIndex == 2) {
+                a = "forras_europa.txt";
+                o_ki.Text = "Európa";
+            }
+            StreamReader sr = new StreamReader(a);
+            
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string[] sor = sr.ReadLine().Split(';');
+                orszag.Add(new orszagok(sor[0], sor[1], Convert.ToInt32(sor[2]), Convert.ToInt32(sor[3]), Convert.ToDouble(sor[4])));
+            }
+            for (int i = 0; i < orszag.Count; i++)
+            {
+                listBox1.Items.Add(orszag[i].Orszag);
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tallozas_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            kiiras.Text = $"Ország: {orszag[listBox1.SelectedIndex].Orszag} Főváros: {orszag[listBox1.SelectedIndex].Fovaros} Terület: {orszag[listBox1.SelectedIndex].Terulet} Népesség: {orszag[listBox1.SelectedIndex].Nepesseg} Népsűrűség: {orszag[listBox1.SelectedIndex].Nepsuruseg}km2";
+        }
+    }
+
+    public class orszagok
+    {
+        public string Orszag { get; set; }
+        public string Fovaros { get; set; }
+        public int Terulet { get; set; }
+        public int Nepesseg { get; set; }
+        public double Nepsuruseg { get; set; }
+
+        public orszagok(string orszag, string fovaros, int terulet, int nepesseg, double nepsuruseg)
+        {
+            Orszag = orszag;
+            Fovaros = fovaros;
+            Terulet = terulet;
+            Nepesseg = nepesseg;
+            Nepsuruseg = nepsuruseg;
+        }
+    }
+}
